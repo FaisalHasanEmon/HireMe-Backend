426cefa41e46373ec2e6271874098531e06c1227
FaisalHasanEmon <faisalhasan5833@gmail.com>
2025-04-12 20:13:54 +0600

    Folder structure created, required packages installed and connection between server to database created

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
+const dbConnect = require("./src/config/dbConnect");
+
+dbConnect();
+
+// Middleware
 app.use(express.json());
 
+// Routes
 app.get("/", (req, res) => {
-  res.send({ name: "Bangladesh" });
+  res.send({ name: "Bangladesh", new: process.env.hello });
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
+  // console.log(process.env.CONNECTION_STRING);
 });
